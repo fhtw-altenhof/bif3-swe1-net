7bf3e83d9eaa6aed2374332d3d0c98b9c92458a0
FireDragonGameStudio <maxaltenhofer@gmx.at>
2020-09-28 21:39:01 +0200

    Fix git history

--- Program.cs
+++ Program.cs
@@ -125,6 +125,8 @@ namespace Bif3.Swe1.Oop {
 
             shapeStack.Pop();
             Console.WriteLine($"Is empty? {shapeStack.IsEmpty()}");
+
+            Console.WriteLine("-----");
         }
     }
 }
