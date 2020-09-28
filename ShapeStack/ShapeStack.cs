9fd19a7ffa658a19cf364e7909a0885ad709acaa
FireDragonGameStudio <maxaltenhofer@gmx.at>
2020-09-28 14:48:37 +0200

    Added empty ShapeStack class

---
+++ ShapeStack/ShapeStack.cs
@@ -0,0 +1,33 @@
+﻿using Bif3.Swe1.Oop.Polymorphism.OptimizedSolution;
+using System;
+using System.Collections.Generic;
+
+namespace Bif3.Swe1.Oop.ShapeStack {
+    class ShapeStack {
+
+        private List<IShapeComposition> _shapeList = new List<IShapeComposition>();
+        private int _top = 0;
+        private int _maxSize = 0;
+
+        public ShapeStack(int maxSize) {
+            _top = 0;
+            _maxSize = maxSize;
+        }
+
+        public void Push(IShapeComposition shapeObj) {
+            throw new NotImplementedException();
+        }
+
+        public Shape Pop() {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty() {
+            throw new NotImplementedException();
+        }
+
+        public void DescribeStack() {
+            throw new NotImplementedException();
+        }
+    }
+}
